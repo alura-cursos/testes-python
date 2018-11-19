047faa5e5b3a71efd2dfba4519eddf27b654620d
Yuri Matheus <yurimatheusb@hotmail.com>
2018-11-19 14:01:44 -0200

    aula-03

---
+++ src/leilao/dominio.py
@@ -0,0 +1,39 @@
+import sys
+
+
+class Usuario:
+
+    def __init__(self, nome):
+        self.__nome = nome
+
+    @property
+    def nome(self):
+        return self.__nome
+
+
+class Lance:
+
+    def __init__(self, usuario, valor):
+        self.usuario = usuario
+        self.valor = valor
+
+
+class Leilao:
+
+    def __init__(self, descricao):
+        self.descricao = descricao
+        self.__lances = []
+        self.maior_lance = sys.float_info.min
+        self.menor_lance = sys.float_info.max
+
+    def propoe(self, lance: Lance):
+        if lance.valor > self.maior_lance:
+            self.maior_lance = lance.valor
+        if lance.valor < self.menor_lance:
+            self.menor_lance = lance.valor
+
+        self.__lances.append(lance)
+
+    @property
+    def lances(self):
+        return self.__lances[:]
---
+++ src/leilao/test_avaliador.py
@@ -0,0 +1,61 @@
+from unittest import TestCase
+from src.leilao.dominio import Usuario, Lance, Leilao
+
+
+class TestAvaliador(TestCase):
+
+    # test_quando_adicionados_em_ordem_crescente_deve_retornar_o_maior_e_o_menor_valor_de_um_lance
+
+    def setUp(self):
+        self.gui = Usuario('Gui')
+        self.lance_do_gui = Lance(self.gui, 150.0)
+        self.leilao = Leilao('Celular')
+
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_crescente(self):
+        yuri = Usuario('Yuri')
+        lance_do_yuri = Lance(yuri, 100.0)
+
+        self.leilao.propoe(lance_do_yuri)
+        self.leilao.propoe(self.lance_do_gui)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 150.0
+
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+
+    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
+        yuri = Usuario('Yuri')
+        lance_do_yuri = Lance(yuri, 100.0)
+
+        self.leilao.propoe(self.lance_do_gui)
+        self.leilao.propoe(lance_do_yuri)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 150.0
+
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+
+    def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_leilao_tiver_um_lance(self):
+        self.leilao.propoe(self.lance_do_gui)
+
+        self.assertEqual(150.0, self.leilao.menor_lance)
+        self.assertEqual(150.0, self.leilao.maior_lance)
+
+    def test_deve_retornar_o_maior_e_o_menor_valor_quando_o_leilao_tiver_tres_lances(self):
+        yuri = Usuario('Yuri')
+        lance_do_yuri = Lance(yuri, 100.0)
+        vini = Usuario('Vini')
+
+        lance_do_vini = Lance(vini, 200.0)
+
+        self.leilao.propoe(lance_do_yuri)
+        self.leilao.propoe(self.lance_do_gui)
+        self.leilao.propoe(lance_do_vini)
+
+        menor_valor_esperado = 100.0
+        maior_valor_esperado = 200.0
+
+        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
+        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
